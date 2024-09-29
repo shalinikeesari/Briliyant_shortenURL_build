39bd530b4764ab908a3e10129bdd24c457d0c33e
shalini keesari <keesarishalini@gmail.com>
2024-09-29 14:29:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UrlShortener from "C:/Users/Shalini/OneDrive/Desktop/briliyantweek1/url-shortener/url-shortener-frontend/src/UrlShortener.js"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>URL Shortener</h1>
+      <UrlShortener /> {/* Use the correct component name */}
+    </div>
+  );
+}
+
+export default App;
